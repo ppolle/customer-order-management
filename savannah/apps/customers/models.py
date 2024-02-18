7d75ab87ddc5fec4eefef663a29b7db8f161778f
ppolle <peter.m.polle@gmail.com>
2024-02-18 16:27:00 +0300

    this commit will implement initial customers functionality

---
+++ savannah/apps/customers/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+# Create your models here.
+
+class Customer(models.Model):
+    """
+    Customer's details
+    """
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    customer_code = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self):
+        super(Customer, self).save()
+        customer_code = f"#CU{self.id}"
+        self.customer_code = customer_code
+        self.save()
+    
