3fa94395c4cd5eae96c7bf324d0e9981ce22b13f
ppolle <peter.m.polle@gmail.com>
2024-02-22 00:06:42 +0300

    this commit will update order model save method

--- savannah/apps/orders/models.py
+++ savannah/apps/orders/models.py
@@ -7,7 +7,7 @@ class Order(models.Model):
     Customer orders
     """
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    order_number = models.CharField(max_length=255)
+    order_number = models.CharField(max_length=255, null=True, blank=True)
     item = models.CharField(max_length=255)
     amount = models.IntegerField()
     time = models.DateTimeField(auto_now=True)
@@ -15,9 +15,8 @@ class Order(models.Model):
     def __str__(self):
         return f"{self.order_number} - {self.customer.name}"
     
-    def save(self):
+    def save(self, *args, **kwargs):
         super(Order, self).save()
         order_number = f"#ON{self.id}"
         self.order_number = order_number
-        self.save()
 
