d4fbc5eb2fb767c8f555c86f72c05f2d17bd0bb1
ppolle <peter.m.polle@gmail.com>
2024-02-21 12:31:02 +0300

    this commit will update the creation of url patterns

--- savannah/apps/customers/api/urls.py
+++ savannah/apps/customers/api/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from savannah.apps.customers.api.viewsets import CustomerViewsets
 
 router = DefaultRouter()
 router.register("customers", CustomerViewsets, basename="customers")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- savannah/apps/orders/api/urls.py
+++ savannah/apps/orders/api/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from savannah.apps.orders.api.viewsets import OrderViewSets
 
 router = DefaultRouter()
 router.register("orders", OrderViewSets, basename="orders")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
