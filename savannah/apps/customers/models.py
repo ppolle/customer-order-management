3c02f0f82951b05ae654e8e40f29c1df1d7451dd
ppolle <peter.m.polle@gmail.com>
2024-03-28 00:23:54 +0300

    this commit will change user field in customer model to one to one field

--- savannah/apps/customers/models.py
+++ savannah/apps/customers/models.py
@@ -8,7 +8,7 @@ class Customer(models.Model):
     """
     Customer's details
     """
-    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    user  = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     customer_code = models.CharField(max_length=255, null=True, blank=True)
 
